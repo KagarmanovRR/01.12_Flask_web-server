eae5d6c0798576d22386e30e02fcedd21bf7961b
KagarmanovRR <KagarmanovRR@example.com>
2022-12-04 00:36:08 +0300

    Добавили картинку

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return "hello world"
 
+@app.route('/image_sample')
+def image():
+    return f"<img src={url_for('static', filename='images/students.jpg')} alt='Картинка'>"
+
 if __name__ == '__main__':
     app.run(port=8081, host='127.0.0.1')
